8e178a5d07fa94db80b59a49e34a42f9bd13da3d
Atharva Hudlikar <36445600+Mastermind0100@users.noreply.github.com>
2019-06-04 18:27:55 +0530

    Update nist_model.py

--- nist_model.py
+++ nist_model.py
@@ -15,6 +15,7 @@ trdata = 66024            # My dataset had these many images. Verify the number 
 vltdata = 12567           # executing lines 33-48 on your compiler
 batch = 32
 
+#The arr_result list may be modified according to the characters that are present in the dataset as independent categories
 arr_result = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 
 training_data = 'nist_new/training'           #This was the location of the dataset on my disk. 
@@ -27,7 +28,8 @@ model.add(Conv2D(32,(3,3),activation='relu'))
 model.add(MaxPooling2D(pool_size=(2,2)))
 model.add(Flatten())
 model.add(Dense(units=128,activation='relu'))
-model.add(Dense(units=26,activation='softmax'))
+
+model.add(Dense(units=26,activation='softmax'))  # The dimentionality of output space depends on the number of characters in question
 model.compile(optimizer='adam',loss='sparse_categorical_crossentropy',metrics=['accuracy'])
 
 train_datagen=ImageDataGenerator(rescale = 1./255,
@@ -49,19 +51,18 @@ test_set=test_datagen.flow_from_directory(directory = validation_data,
 
 model.fit_generator(training_set,steps_per_epoch = 2000,         
                          epochs = 10,
-                         validation_data = test_set,
+                         validation_data = test_set,             #vldata//batch = 400
                          validation_steps = 400)                 #trdata//batch = 2000
-                                                                 #vldata//batch = 400
-
+                                                                 
 model.save('modelwts.h5')
 #Model Creation and Training code ends here
 
+
 #This is the code for testing
 from keras.preprocessing import image
-
 model=load_model('modelwts.h5')
 
-test_image=image.load_img('Q.png', target_size = (64, 64))
+test_image=image.load_img('test_image.png', target_size = (64, 64))     #Name and directory location of the image being tested
 test_image=(image.img_to_array(test_image))/255
 test_image=np.expand_dims(test_image, axis = 0)
 result=model.predict(test_image)
@@ -70,4 +71,6 @@ training_set.class_indices
   
 maxval = np.amax(result)
 index = np.where(result == maxval)
+
+#Final Output of Prediction
 print('\n','Predicted Character:',arr_result[index[1][0]])        
